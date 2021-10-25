70e8834e44cf3d006f599ad5b632d99f2f305cf9
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2021-10-25 21:57:57 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 // 
+function startup() {
+    coverForStart();
+    typewriter.type();
+} 
+
+// 01 cover for start
+function coverForStart () {
+    const infoMarquee = document.querySelector('.info-section .marquee .cover-for-start');
+    infoMarquee.style.display = 'block';
+    setTimeout(() => {
+        infoMarquee.style.display = 'none';
+    }, 4520)
+
+    const infoContainer = document.querySelector('.info-section .info-container .cover-for-start');
+    infoContainer.style.display = 'block';
+    setTimeout(() => {
+        infoContainer.style.display = 'none';
+    }, 4040)
+
+    const subNav = document.querySelector('.subnav .cover-for-start');
+    subNav.style.display = 'block';
+    setTimeout(() => {
+        subNav.style.display = 'none';
+    }, 4940)
+}
 
 // hide navbar on scroll
 var prevScrollpos = window.pageYOffset;
@@ -52,31 +77,41 @@ zoomOutTool.addEventListener('click', () => {
     zooming();
 })
 
-// 01 cover for start
-function coverForStart () {
-    const infoMarquee = document.querySelector('.info-section .marquee .cover-for-start');
-    infoMarquee.style.display = 'block';
-    setTimeout(() => {
-        infoMarquee.style.display = 'none';
-    }, 4520)
+// 02 rotate logo
+const logo = document.getElementById('logo');
 
-    const infoContainer = document.querySelector('.info-section .info-container .cover-for-start');
-    infoContainer.style.display = 'block';
-    setTimeout(() => {
-        infoContainer.style.display = 'none';
-    }, 4040)
+function rotateLogo () {
+    let angle;
 
-    const subNav = document.querySelector('.subnav .cover-for-start');
-    subNav.style.display = 'block';
-    setTimeout(() => {
-        subNav.style.display = 'none';
-    }, 4940)
+    window.addEventListener('mousemove', e => {
+        logo.style.transition = 'none';
+        
+        if (e.clientX < 600 && e.clientY < 500 && e.clientX > 0 && e.clientY > 0) {
+            let angleRadians = Math.atan2(e.clientY - 46, e.clientX - 37.5);
+            angle = Math.round(angleRadians * 180 / Math.PI);
+
+        } else {
+            if (angle > 36) {
+                angle = angle - 0.2
+            } else if (angle < 36) {
+                angle = angle + 0.2
+            } else {
+                angle = 36
+            }
+            // angle = 36
+        }
+        logo.style.transform = 'rotateZ(' + angle + 'deg)';
+    })
+
+    document.addEventListener('mouseleave', () => {
+        logo.style.transition = 'transform 0.3s';
+        logo.style.transform = 'rotateZ(36deg)';
+    })
 }
 
-// coverForStart();
-window.onload = coverForStart;
+rotateLogo();
 
-// 02 clock
+// 03 clock
 
 const clock = {
     logTime: function logTime () {
@@ -117,7 +152,7 @@ const clock = {
 
 clock.logTime();
 
-// 03 typing and no drawing
+// 04 typing and no drawing
 
 const mynameTyping = 'tran trân trần thanh thao thảo';
 const myname = 'trần thanh thảo';
@@ -282,9 +317,9 @@ const typewriter = {
     }
 }
 
-typewriter.type();
+// 05 display avatar
 
-// 04 darkenButton only info-button
+// 06 darkenButton only info-button
 function darkenButton () {
     const infoButton = document.querySelector('.button-info.dark-button-info');
 
@@ -303,3 +338,160 @@ function darkenButton () {
 }
 
 darkenButton();
+
+// item2 
+const solowContainer = document.querySelector('.item2');
+const solowCopies = document.getElementsByClassName('solow-copy');
+
+function solow() {
+    var rect = solowContainer.getBoundingClientRect();
+        x = rect.left;
+        y = rect.top;
+        w = rect.width;
+        h = rect.height;
+
+    var a = 240;
+
+    var currentEvent, prevEvent;
+    solowContainer.addEventListener('mousemove', e => {
+        currentEvent = e
+    })
+
+    solowContainer.addEventListener('touchstart', () => {
+        for (let i = 0; i < solowCopies.length; i++) {
+            let randomX = Math.floor(15 + Math.random() * (w - a - 30));
+            let randomY = Math.floor(15 + Math.random() * (h - a - 30));
+
+            solowCopies[i].style.top = randomY + "px";
+            solowCopies[i].style.left = randomX + "px";
+        }
+    })
+
+    solowContainer.addEventListener('mouseleave', () => {
+        setTimeout(() => {    
+            solowCopies[2].style.top = '180px';
+            solowCopies[2].style.left = '130px';
+            solowCopies[2].style.transform = 'rotateZ(-8deg)';
+        }, 650)
+    })
+
+    solowContainer.addEventListener('mouseenter', () => {
+        solowCopies[0].style.top = '90px';
+        solowCopies[0].style.left = '190px';
+        solowCopies[0].style.transform = 'rotateZ(8deg)';
+
+        solowCopies[1].style.top = '130px';
+        solowCopies[1].style.left = '160px';
+        solowCopies[1].style.transform = 'rotateZ(0)';
+
+        solowCopies[2].style.top = '180px';
+        solowCopies[2].style.left = '130px';
+        solowCopies[2].style.transform = 'rotateZ(-8deg)';
+
+        setTimeout(() => {
+            solowRandom();
+        }, 500)
+    })
+
+    solowContainer.addEventListener('mousedown', (e) => {
+        currentEvent = e;
+        solowCopies[2].style.top = (currentEvent.clientY - y) - a/2 + "px";
+        solowCopies[2].style.left = (currentEvent.clientX - x) - a/2 + "px";
+
+        if (currentEvent.clientX < x + a/2 + 14 && currentEvent.clientY < y + a/2 + 14) { //corner top left
+            solowCopies[2].style.left = '14px';
+            solowCopies[2].style.top = '14px';
+        } else if (currentEvent.clientX > x + w - a/2 -14 && currentEvent.clientY < y + a/2 + 14) { //corner top right
+            solowCopies[2].style.left = w - a - 14 + 'px';
+            solowCopies[2].style.top = '14px';
+        } else if (currentEvent.clientX < x + a/2 + 14 && currentEvent.clientY > y + h - a/2 - 14) { //corner bottom left
+            solowCopies[2].style.left = '14px';
+            solowCopies[2].style.top = h - a - 14 + 'px';
+        } else if (currentEvent.clientX > x + w - a/2 -14 && currentEvent.clientY > y + h - a/2 - 14) { //corner bottom right
+            solowCopies[2].style.left = w - a - 14 + 'px';
+            solowCopies[2].style.top = h - a - 14 + 'px';
+        } else if (currentEvent.clientX < x + a/2) { //edge left
+            solowCopies[2].style.left = '14px';
+            solowCopies[2].style.top = (currentEvent.clientY - y) - a/2 + "px";
+        } else if (currentEvent.clientX > x + w - a/2 - 14) { //edge right
+            solowCopies[2].style.left = w - a - 14 + 'px';
+            solowCopies[2].style.top = (currentEvent.clientY - y) - a/2 + "px";
+        } else if (currentEvent.clientY < y + a/2 + 14) { //edge top
+            solowCopies[2].style.left = (currentEvent.clientX - x) - a/2 + "px";
+            solowCopies[2].style.top = '14px';
+        } else if (currentEvent.clientY > y + h - a/2 - 14) { //edge bottom
+            solowCopies[2].style.left = (currentEvent.clientX - x) - a/2 + "px";
+            solowCopies[2].style.top = h - a - 14 + 'px';
+        } 
+
+    })
+
+    function solowRandom() {
+        let delay = 0;
+
+        setInterval(() => {
+            if (currentEvent && prevEvent) {
+                var movementX = currentEvent.clientX - prevEvent.clientX;
+                var movementY = currentEvent.clientY - prevEvent.clientY;
+                var movement = Math.sqrt(movementX * movementX + movementY * movementY);
+
+                if (movement > 20 && currentEvent.clientX > x + w/2 && currentEvent.clientY > y + h/2) {
+                    setTimeout(() => {
+                        for (let i = 0; i < solowCopies.length; i++) {
+                            let randomX;
+                            let randomY = Math.floor(15 + Math.random() * (h - a - 30));
+
+                            if (randomY < h/2 - a - 30) {
+                                randomX = Math.floor(15 + Math.random() * (w - a - 30))
+                            } else {
+                                randomX = Math.floor(15 + Math.random() * (w/2 - a - 30))
+                            }
+
+                            solowCopies[i].style.top = randomY + "px";
+                            solowCopies[i].style.left = randomX + "px";
+                        }
+                    }, delay)
+                    
+                } else if (movement > 20 && currentEvent.clientX > x + w/2 && currentEvent.clientY < y + h/2) {
+                    setTimeout(() => {
+                        for (let i = 0; i < solowCopies.length; i++) {
+                            let randomX = Math.floor(15 + Math.random() * (w - a - 30));
+                            let randomY;
+
+                            if (randomX < w/2 - a - 30) {
+                                randomY = Math.floor(15 + Math.random() * (h - a - 30))
+                            } else {
+                                randomY = Math.floor(h/2 + Math.random() * (h/2 - a - 30))
+                                console.log('hm')
+                            }
+                            solowCopies[i].style.top = randomY + "px";
+                            solowCopies[i].style.left = randomX + "px";
+                        }
+                    }, delay)
+                } else if (movement > 20) {
+                    setTimeout(() => {
+                        for (let i = 0; i < solowCopies.length; i++) {
+                            let randomX = Math.floor(15 + Math.random() * (w - a - 30));
+                            let randomY = Math.floor(15 + Math.random() * (h - a - 30));
+
+                            solowCopies[i].style.top = randomY + "px";
+                            solowCopies[i].style.left = randomX + "px";
+                        }
+                    }, delay)
+                }      
+            }
+
+            prevEvent = currentEvent;
+        }, 600)
+    }
+}
+
+solow();
+
+
+
+
+
+
+// last
+document.addEventListener('DOMContentLoaded', startup);
